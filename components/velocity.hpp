728326e270943adebf98fa47f3cf993a9d4511c9
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-21 08:38:35 +0800

    Update velocity.hpp

--- components/velocity.hpp
+++ components/velocity.hpp
@@ -1,14 +1,8 @@
 #pragma once
 #include <glm/glm.hpp>
 
-namespace Components {
-
-        /**
-         * @struct Velocity
-         * @brief Represents the linear physical movement vector of an entity.
-         */
+namespace Components{
     struct Velocity{
-
         glm::vec3 Linear{0.0f};
     };
 }
