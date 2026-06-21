d3e5d2de10df4475d6aca966dd1196d73412e026
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-21 08:14:34 +0800

    Update transform.hpp

--- components/transform.hpp
+++ components/transform.hpp
@@ -1,13 +1,14 @@
 #pragma once
 #include <glm/glm.hpp>
 
-namespace Components {
-
+namespace Components 
+{
         /**
          * @struct Transform
          * @brief Represents the spatial state of an entity in 3D space.
          */
-    struct Transform {
+    struct Transform
+    {
         glm::vec3 Position{0.0f};
         float Rotation{0.0f};
     };
